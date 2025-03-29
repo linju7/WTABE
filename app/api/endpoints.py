30d496b9cc387dc118cebf2e3625d1fa2e3b0d26
노준일 <nojun-il@nojun-il-ui-MacBookAir.local>
2025-03-29 22:44:07 +0900

    cors 설정 + 요청에 대한 응답이 된다는 점 확인

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter
-from app.services.selenium_service import run_selenium_task
-from app.schemas import ResponseSchema
 
 router = APIRouter()
 
-@router.post("/create/internal_group", response_model=ResponseSchema)
+@router.post("/api/create/internal_group")
 async def create_internal_group():
     try:
-        # 셀레니움 작업을 실행
-        result = await run_selenium_task()
 
         # 성공한 경우 결과를 반환
-        return {"message": "Internal group created successfully", "data": result}
+        return {"message": "요청 성공"}
 
     except Exception as e:
         # 실패한 경우 오류 메시지 반환
