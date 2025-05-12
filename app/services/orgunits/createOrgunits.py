961f298a167c04d29717a5e2448b392004bfc296
USER <user@AL02529596-99.local>
2025-05-12 20:42:58 +0900

    로그인 API 정비

---
+++ app/services/orgunits/createOrgunits.py
@@ -0,0 +1,16 @@
+
+
+async def process_create_orgunits(page, instance, server) : 
+    """
+    조직 생성 프로세스
+    """
+    
+    # 1. 어드민 > 조직 페이지로 이동
+    
+    # 2. 조직 추가 버튼 클릭 
+    
+    # 3. 조직 정보 입력 
+    
+    # 4. 확인 버튼 클릭 
+    
+    # 5. 결과 반환 
